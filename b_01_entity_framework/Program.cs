48da39eefb8e555f6a3179e077db11a940b20886
Song Li <da_tou_li@yahoo.com>
2020-11-06 11:31:13 -0500

    Experimentaing

--- b_01_entity_framework/Program.cs
+++ b_01_entity_framework/Program.cs
@@ -1,7 +1,12 @@
 ﻿using dotenv.net;
 
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using b_01_entity_framework.Example_1;
+using b_01_entity_framework.Example_2;
 
 namespace b_01_entity_framework
 {
@@ -14,10 +19,31 @@ namespace b_01_entity_framework
             Console.WriteLine($"ID = {s.ID}, Name = {s.Name}");
           }
         }
+
+        private static async Task Example_2_Load_UnusedDataOrder() {
+          var orderId = 87746;
+          List<UnusedDataOrder> data = null;
+
+          using (var context = new Example2Context())
+          {
+            data = await context.UnusedDataOrder
+            .Where( x => x.OrderId == orderId )
+            .ToListAsync();
+          }
+
+          foreach (var d in data) {
+            Console.WriteLine($"ID = {d.CorelogicDataId}, Name = {d.ProgramTypeId}");
+          }
+
+          Console.WriteLine($"Total count = {data.Count()}");
+        }
+
         static void Main(string[] args)
         {
           DotEnv.Config();
-          Example_1();
+
+          //Example_1();
+          Example_2_Load_UnusedDataOrder().Wait();
         }
     }
 }
