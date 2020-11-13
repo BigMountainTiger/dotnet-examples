6d789ee466443b1502ba05211eb9b215d379a115
Song Li <da_tou_li@yahoo.com>
2020-11-13 16:33:25 -0500

    Experiment on aggregation

--- b_01_entity_framework/Example_2/Models/A_Example2_JOIN_Test.cs
+++ b_01_entity_framework/Example_2/Models/A_Example2_JOIN_Test.cs
@@ -18,14 +18,31 @@ namespace b_01_entity_framework.Example_2 {
           .Where(x => TestID.Test_IDS.Contains(x.CorelogicDataID))
           .Join(context.DataProgramtype.Where(x => x.ProgramTypeID == 6),
             d => new { d.CorelogicDataID }, p => new { p.CorelogicDataID }, (d, p) => d
+          ).GroupJoin(context.UnusedDataOrder
+              .Where(i => i.ProgramTypeId == 6)
+              .Select(i => new { i.CorelogicDataId, i.DropDate }),
+                d => d.CorelogicDataID, o => o.CorelogicDataId,
+                (data, dataOrders) => new { data, dataOrders }
+          ).SelectMany(i => i.dataOrders.DefaultIfEmpty(),
+              (aggregate, dataOrder) => new {
+                aggregate.data.CorelogicDataID,
+                DropDate = (DateTime?)dataOrder.DropDate
+              }
+          ).GroupBy(i => new { i.CorelogicDataID },
+            i => i.DropDate
+          ).Select(i => new {
+              i.Key.CorelogicDataID,
+              UsageCount = i.Count(),
+              LastUsedDate = i.Max()
+            }
           );
 
         Console.WriteLine(query.ToQueryString());
         var data = await query.ToListAsync();
 
-        // foreach(var d in data) {
-        //   Console.WriteLine($"{d.CorelogicDataID}");
-        // }
+        foreach(var d in data) {
+          Console.WriteLine($"{d.CorelogicDataID} - {d.UsageCount} - {d.LastUsedDate}");
+        }
 
         Console.WriteLine($"Count = {data.Count()}");
       }
