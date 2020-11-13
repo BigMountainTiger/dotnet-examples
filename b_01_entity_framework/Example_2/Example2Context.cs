73c1ca3675d73b0de9ec83952b36786d25b3bf14
Song Li <da_tou_li@yahoo.com>
2020-11-13 09:28:20 -0500

    Added DataProgramtype model

--- b_01_entity_framework/Example_2/Example2Context.cs
+++ b_01_entity_framework/Example_2/Example2Context.cs
@@ -5,10 +5,17 @@ namespace b_01_entity_framework.Example_2 {
   public class Example2Context : DbContext {
     public DbSet<UnusedDataOrder> UnusedDataOrder { get; set; }
     public DbSet<UnusedData> UnusedData { get; set; }
+    public DbSet<DataProgramtype> DataProgramtype { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       var constr = Environment.GetEnvironmentVariable("Example2-CSTR");
       optionsBuilder.UseSqlServer(constr);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DataProgramtype>()
+            .HasKey(t => new { t.CorelogicDataID, t.ProgramTypeID });
     }
   }
 }
---
+++ b_01_entity_framework/Example_2/Models/DataProgramtype.cs
@@ -0,0 +1,6 @@
+namespace b_01_entity_framework.Example_2 {
+  public class DataProgramtype {
+    public int CorelogicDataID { get; set; }
+    public int ProgramTypeID { get; set; }
+  }
+}
