2e6ad6b787178887c922bc3a68a2228a23cd9024
Song Li <da_tou_li@yahoo.com>
2020-11-13 13:57:35 -0500

    Keep the temporary

--- b_01_entity_framework/Example_2/Models/A_Example2_JOIN_Test.cs
+++ b_01_entity_framework/Example_2/Models/A_Example2_JOIN_Test.cs
@@ -10,23 +10,31 @@ namespace b_01_entity_framework.Example_2 {
   public class A_Example2_JOIN_Test {
 
     public static async Task Test() {
-      List<DataProgramtype> data = null;
 
       using (var context = new Example2Context())
       {
-        var query = context.DataProgramtype
-        .Where(x => TestID.Test_IDS.Contains(x.CorelogicDataID))
-        .Where(x => x.ProgramTypeID == 6);
+
+        var query = context.UnusedData
+          .Where(x => TestID.Test_IDS.Contains(x.CorelogicDataID));
+
+        // Join(context.DataProgramtype.Where(i => i.ProgramTypeID == model.ProgramTypeId).Select(i => i.CorelogicDataID),
+        //             data => data.CorelogicDataID,
+        //             dataProgramtype => dataProgramtype,
+        //             (data, dataProgramtype) => data)
 
         // Console.WriteLine(query.ToQueryString());
-        data = await query.ToListAsync();
+        var data = await query
+          .Select( x => new {
+            x.CorelogicDataID
+          }).ToListAsync();
+
+        foreach(var d in data) {
+          Console.WriteLine($"{d.CorelogicDataID}");
+        }
+
+        Console.WriteLine($"Count = {data.Count()}");
       }
 
-      foreach (var d in data) {
-        Console.WriteLine($"ID = {d.CorelogicDataID}, F-Name = {d.ProgramTypeID}");
-      }
-
-      Console.WriteLine($"Total count = {data.Count()}");
     }
 
   }
