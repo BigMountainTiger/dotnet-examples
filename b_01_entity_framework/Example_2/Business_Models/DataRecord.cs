bdff913ed5a0ed097d78386cb77744f5a4ae3580
Song Li <da_tou_li@yahoo.com>
2020-11-16 16:11:57 -0500

    Keep a record

--- b_01_entity_framework/Example_2/Business_Models/DataRecord.cs
+++ b_01_entity_framework/Example_2/Business_Models/DataRecord.cs
@@ -10,3 +10,44 @@ namespace b_01_entity_framework.Example_2.Business_Models {
   }
 
 }
+
+// public sealed class DataRecord
+//     {
+//         public UnusedDataDto Data { get; }
+//         public List<OrderDetailSurplus> EligibleOrderDetails { get; } = new List<OrderDetailSurplus>();
+//         public List<OrderDetailSurplus> EligibleNonOwnerOrderDetails { get; private set; } = new List<OrderDetailSurplus>();
+//         public OrderDetailSurplus Owner { get; private set; }
+
+//         public DataRecord(UnusedDataDto data)
+//         {
+//             Data = data;
+//         }
+
+//         public void SetOwner(OrderDetailSurplus owner)
+//         {
+//             Owner = owner;
+//             EligibleNonOwnerOrderDetails = EligibleOrderDetails.Except(new[] { owner }).ToList();
+//         }
+//     }
+
+// -----
+
+// public sealed class DataRecordCollection
+//     {
+//         private readonly Dictionary<int, DataRecord> _records = new Dictionary<int, DataRecord>();
+
+//         public List<DataRecord> GetAllRecords()
+//         {
+//             return new List<DataRecord>(_records.Select(i => i.Value));
+//         }
+
+//         public DataRecord AddToCollection(DataRecord record)
+//         {
+//             if (_records.ContainsKey(record.Data.CorelogicDataId))
+//                 return _records[record.Data.CorelogicDataId];
+
+//             _records.Add(record.Data.CorelogicDataId, record);
+
+//             return record;
+//         }
+//     }
